392e7beb7fb28e129dd030b3223bf7d8c84f4783
Apavayan <info@apavayan.com>
2016-05-19 13:40:31 +0530

    a8elte: Use better Artwork
    Signed-off-by: Apavayan <info@apavayan.com>

--- device_a8elte.mk
+++ device_a8elte.mk
@@ -73,12 +73,10 @@ PRODUCT_COPY_FILES += \
 ### GRAPHICS
 ###########################################################
 
-# This device is xhdpi.  However the platform doesn't
-# currently contain all of the bitmaps at xhdpi density so
-# we do this little trick to fall back to the hdpi version
-# if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := 480dpi
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=196608
