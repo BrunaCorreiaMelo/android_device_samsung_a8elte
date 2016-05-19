f3e248ac2d8857a430e3e7214a584abd430bfc3c
Apavayan <info@apavayan.com>
2016-05-19 13:28:09 +0530

    a8elte: Copy Audio policy and Mixer path to system
    Signed-off-by: Apavayan <info@apavayan.com>

--- device_a8elte.mk
+++ device_a8elte.mk
@@ -10,6 +10,15 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/a8elte/overlay
 LOCAL_PATH := device/samsung/a8elte
 
 ###########################################################
+### Audio
+##########################################################
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+
+###########################################################
 ### RAMDISK
 ###########################################################
 
